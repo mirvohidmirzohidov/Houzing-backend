2ab9931f174c46f40cc6e75c688f15809d7c4e94
mirvohidmirzohidov <mirvohidmirzohidov001@gmail.com>
2025-02-26 20:28:33 +0500

    server.js sozlandi

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Backend ishlayapti! 🚀");
+  });
+  
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
